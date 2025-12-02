6d50d058c82e2f70c87032f72ad1016c852bc019
Clement Guillot <clement.guillot@enpc.fr>
2025-12-02 13:33:51 +0100

    Implemented NullNumber

---
+++ src/NullNumbers.jl
@@ -0,0 +1,52 @@
+module NullNumbers
+
+export NullNumber
+export imagz
+
+struct NullNumber <: Number end
+
+# +
+@inline Base.:+(::NullNumber, ::NullNumber) = NullNumber()
+@inline Base.:+(::NullNumber, x::Number) = x
+@inline Base.:+(x::Number, ::NullNumber) = x
+# -
+@inline Base.:-(::NullNumber) = NullNumber()
+@inline Base.:-(::NullNumber, ::NullNumber) = NullNumber()
+@inline Base.:-(::NullNumber, x::Number) = -x
+@inline Base.:-(x::Number, ::NullNumber) = x
+# *
+@inline Base.:*(::NullNumber, ::NullNumber) = NullNumber()
+@inline Base.:*(::NullNumber, ::Number) = NullNumber()
+@inline Base.:*(::Number, ::NullNumber) = NullNumber()
+# /
+@inline Base.:/(::NullNumber, ::NullNumber) = throw(DivideError())
+@inline Base.:/(::NullNumber, ::Number) = NullNumber()
+@inline Base.:/(::Number, ::NullNumber) = throw(DivideError())
+# \
+@inline Base.:\(::NullNumber, ::NullNumber) = throw(DivideError())
+@inline Base.:\(::Number, ::NullNumber) = NullNumber()
+@inline Base.:\(::NullNumber, ::Number) = throw(DivideError())
+# ^
+@inline Base.:^(::NullNumber, ::NullNumber) = throw(DomainError("NullNumber^NullNumber is undefined"))
+@inline Base.:^(::NullNumber, ::Number) = NullNumber()
+@inline Base.:^(::NullNumber, ::Integer) = NullNumber()
+@inline Base.:^(::T, ::NullNumber) where T<:Number = one(T)
+
+@inline Base.sqrt(::NullNumber) = NullNumber()
+@inline Base.abs(::NullNumber) = NullNumber()
+@inline Base.abs2(::NullNumber) = NullNumber()
+@inline Base.exp(::NullNumber) = true
+@inline Base.exp2(::NullNumber) = true
+@inline Base.cis(::NullNumber) = true
+
+@inline Base.hypot(::NullNumber, ::NullNumber) = NullNumber()
+
+@inline imagz(::Union{Real, NullNumber}) = NullNumber()
+@inline imagz(z::Complex) = imag(z)
+
+@inline Base.zero(::Type{NullNumber}) = NullNumber()
+@inline Base.zero(::NullNumber) = NullNumber()
+Base.convert(::Type{T}, ::NullNumber) where T<:Number = zero(T)
+
+end # module NullNumber
+
